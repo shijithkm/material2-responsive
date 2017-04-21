b4b0fb317a5e2b7ee53d71a36b358db56ae615d2
Shijith <mail.shijith@gmail.com>
2017-04-22 01:29:30 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {MdIconRegistry, MdDialog} from '@angular/material';
+import {DomSanitizer} from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+isDarkTheme = false;
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+import {AppComponent} from './app.component';
+
+
+import 'hammerjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
